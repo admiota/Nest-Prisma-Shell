67271bf960aec89cf278dc2fed97cd2ceb346f06
adolfo <adolfomiota1@gmail.com>
2024-03-25 10:30:26 +0100

    ajuste

--- src/seed/data/seed-data.ts
+++ src/seed/data/seed-data.ts
@@ -44,7 +44,7 @@ export const initialData: SeedData = {
         }
     ],
     products: [
-        {
+        /*{
             description: "Introducing the Tesla Chill Collection. The Men’s Chill Crew Neck Sweatshirt has a premium, heavyweight exterior and soft fleece interior for comfort in any season. The sweatshirt features a subtle thermoplastic polyurethane T logo on the chest and a Tesla wordmark below the back collar. Made from 60% cotton and 40% recycled polyester.",
             images: [
                 '1740176-00-A_0_2000.jpg',
@@ -772,6 +772,6 @@ export const initialData: SeedData = {
             tags: ['shirt'],
             title: "Kids Corp Jacket",
             gender: 'kid'
-        },
+        },*/
     ]
 }
